b1f8b644816256f8ab8e11da8494934736d076bb
Peter Garvin <peter.garvin@mandg.co.uk>
2019-04-04 13:12:24 +0100

    first commit

---
+++ LeapYearKata.Source/LeapYearService.cs
@@ -0,0 +1,21 @@
+﻿namespace LeapYearKata.Source
+{
+    public class LeapYearService
+    {
+        public bool IsLeapYear(int year)
+        {
+            if (year % 100 == 0 && year % 400 != 0)
+            {
+                return false;
+            }
+
+            if (year % 4 == 0)
+            {
+                return true;
+            }
+
+            return false;
+
+        }
+    }
+}
---
+++ LeapYearKata.Tests/LeapYearServiceShould.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using FluentAssertions;
+using LeapYearKata.Source;
+using NUnit.Framework;
+
+namespace LeapYearKata.Tests
+{
+    [TestFixture]
+    public class LeapYearServiceShould
+    {
+        private readonly LeapYearService _leapYearService = new LeapYearService();
+
+        [TestCase(16, true)]
+        [TestCase(9, false)]
+        [TestCase(41, false)]
+        [TestCase(63, false)]
+        [TestCase(100, false)]
+        [TestCase(200, false)]
+        [TestCase(300, false)]
+        [TestCase(400, true)]
+        [TestCase(800, true)]
+        [TestCase(1200, true)]
+        public void ReturnCorrectValue_WhenChecking(int year, bool expectedResult)
+        {
+            _leapYearService
+                .IsLeapYear(year)
+                .Should().Be(expectedResult);
+        }
+        
+    }
+}
